ef897ff0f7346fc185ff8de017d39f05a29509f7
samantha lin <samanthalin1992@gmail.com>
2017-06-05 00:27:10 +0800

    index screen-xs done

---
+++ sketch.js
@@ -0,0 +1,43 @@
+var yoff = 0.0;        // 2nd dimension of perlin noise
+
+function setup() {
+  canvas = createCanvas(windowWidth, 400);
+  canvas.parent("animation");
+}
+
+function draw() {
+  background(241);
+
+  fill(25, 202, 144);
+  noStroke();
+  // We are going to draw a polygon out of the wave points
+  beginShape(); 
+  
+  var xoff = 0;       // Option #1: 2D Noise
+  // var xoff = yoff; // Option #2: 1D Noise
+  
+  // Iterate over horizontal pixels
+  for (var x = 0; x <= width; x += 10) {
+    // Calculate a y value according to noise, map to 
+    
+    // Option #1: 2D Noise
+    var y = map(noise(xoff, yoff), 0, 1, 200,300);
+
+    // Option #2: 1D Noise
+    // var y = map(noise(xoff), 0, 1, 200,300);
+    
+    // Set the vertex
+    vertex(x, y); 
+    // Increment x dimension for noise
+    xoff += 0.05;
+  }
+  // increment y dimension for noise
+  yoff += 0.01;
+  vertex(width, height);
+  vertex(0, height);
+  endShape(CLOSE);
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, 400);
+}
